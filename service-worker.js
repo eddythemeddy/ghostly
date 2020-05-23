671bd21f8bb53bab734f31eac42524eeb06d8e47
DanielBaker13 <danielbakerr13@gmail.com>
2020-05-23 14:49:56 -0400

    Daniels commit to prod

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2e46aa559f06654ba942e9596d8e90c1.js"
+  "./precache-manifest.01a7220daae760c8e661fb6074402a3b.js"
 );
 
 self.addEventListener('message', (event) => {
